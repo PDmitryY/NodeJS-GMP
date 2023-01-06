65637bdcfdc976ea99bca2637b6e39ac46e1fd61
Dmitry <p.dmitry.y@gmail.com>
2023-01-06 03:19:49 +0300

    refactor: place homework1 to separete folder

--- homework1/task2.js
+++ homework1/task2.js
@@ -8,10 +8,10 @@ const __dirname = path.resolve();
 async function convertCsvToTxt () {
   try {
     const csvPath = path.join(__dirname, 'csv', 'nodejs-hw1-ex1.csv');
-    fs.mkdir(path.join(__dirname, 'txt'), { recursive: true }, (err) => {
+    fs.mkdir(path.join(__dirname, 'homework1', 'txt'), { recursive: true }, (err) => {
       if (err) throw err;
     });
-    const txtPath = path.join(__dirname, 'txt', 'nodejs-hw1-ex1.txt');
+    const txtPath = path.join(__dirname, 'homework1', 'txt', 'nodejs-hw1-ex1.txt');
 
     const readStream=fs.createReadStream(csvPath);
 
